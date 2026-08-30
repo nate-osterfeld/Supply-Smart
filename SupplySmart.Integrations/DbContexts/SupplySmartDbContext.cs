b34a50cb03f5c8fd634b9f73beef34876ffb16ba
Nate Osterfeld <osterfnl@mail.uc.edu>
2026-08-29 23:42:57 -0400

    Install Core packages, add SupplyItem model, create DbContext, and wire local connection string

---
+++ SupplySmart.Integrations/DbContexts/SupplySmartDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupplySmart.Integrations.Models;
+
+namespace SupplySmart.Integrations.DbContexts;
+
+public class SupplySmartDbContext : DbContext
+{
+    public SupplySmartDbContext(DbContextOptions<SupplySmartDbContext> options) : base(options) { }
+
+    public DbSet<SupplyItem> SupplyItems => Set<SupplyItem>();
+}
---
+++ SupplySmart.Integrations/Models/SupplyItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SupplySmart.Integrations.Models;
+
+public class SupplyItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Sku { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+}
